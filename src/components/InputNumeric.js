105e1a4820afff50c623fcb5479185703cac55f8
Patrick Edqvist <patrick.edqvist@gmail.com>
2017-10-24 22:43:17 +0200

    Add component InputNumeric

---
+++ src/components/InputNumeric.js
@@ -0,0 +1,101 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+// Helpers
+import { toNumericValue } from '../helpers/format';
+
+class InputNumeric extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.defaultValue && !props.value ? props.defaultValue : props.value
+    }
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnFocus = this.handleOnFocus.bind(this);
+    this.handleOnBlur = this.handleOnBlur.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if ( nextProps.value ) {
+      this.setState(() => ({ value: nextProps.value }));
+    }
+  }
+
+  handleOnChange(event) {
+    const value = event.target.value;
+    this.setState(
+      () => ({ value }),
+      () => this.props.onChange(event)
+    );
+  }
+
+  handleOnFocus(event) {
+    this.props.onFocus(event);
+  }
+
+  handleOnBlur(event) {
+    this.props.onBlur(event);
+  }
+
+  render() {
+
+    // Render label if the label prop is truthy
+    const renderLabel = this.props.label ? <label htmlFor={this.props.name}>{this.props.label}</label> : null;
+
+    // Render error if the error props is truthy
+    const renderError = this.props.error ? <span>{this.props.error}</span> : null;
+
+    return (
+      <div className={this.props.className}>
+        {renderLabel}
+        <input
+          type="text"
+          id={this.props.name}
+          name={this.props.name}
+          value={toNumericValue(this.props.value)}
+          placeholder={this.props.placeholder}
+          onChange={this.handleOnChange}
+          onFocus={this.handleOnFocus}
+          onBlur={this.handleOnBlur}
+        />
+        {renderError}
+      </div>
+    );
+  }
+}
+
+
+InputNumeric.defaultProps = {
+  label: '',
+  value: '',
+  defaultValue: '',
+  name: '',
+  placeholder: '',
+  className: 'form-group form-group--InputNumeric',
+  error: '',
+  onChange: () => {},
+  onFocus: () => {},
+  onBlur: () => {}
+};
+
+InputNumeric.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  defaultValue: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  className: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func
+}
+
+export default InputNumeric;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as InputText } from './components/InputText';
+export { default as InputNumeric } from './components/InputNumeric';
